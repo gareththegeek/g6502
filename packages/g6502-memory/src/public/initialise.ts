f27341730f0d167eb14268a2ced9513f07f80dbc
Gareth Williams <gwilliams@microsoft.com>
2022-02-03 11:55:04 +0000

    Exclude logs and remove obsolete initialise function

--- packages/g6502-memory/src/public/initialise.ts
+++
@@ -1,6 +0,0 @@
-import { Store } from 'g6502-interfaces'
-import State from '../State'
-import { buildInitialiseEvent } from '../events/buildDefaultStateEvent'
-
-export const initialise = (store: Store<State>, pageCount: number): void =>
-    store.write(buildInitialiseEvent(pageCount))
